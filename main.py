64d762654193ef96de0606f5580b9671485a0e48
guanhua202 <k.kalashnikova0301@gmail.com>
2026-09-18 22:35:59 +0300

    New functions

--- main.py
+++ main.py
@@ -1,108 +1,81 @@
-# ————— 🗂️ Органайзер файлов
-# ✅ Принимать путь рабочей папки через sys.argv[1]
-# o 1. Получить дату/расширение/размер подпапок
-# ✅ Дать возможность выводить список подпапок
-# o 3. Дать возможность отфильтровывать вывод подпапок по 3-м опциям: дата/расширение/размер
-# 	✅ По расширению
-# 	o По размеру
-# 	o По дате
+# ———————————————————— 🗂️ Органайзер файлов
+# o 2. Дать возможность выводить список подпапок
+# ✅ 3. Дать возможность отфильтровывать вывод подпапок по 3-м опциям: дата/расширение/размер
+	# 	✅ По расширению
+	# 	✅ По размеру
+	# 	✅ По дате
 
 import os
 import sys
 from time import sleep
 from time import ctime
 
-print('\n—————————— 🗂️ Органайзер файлов\n')
+print('\n—————————————— 🗂️ Органайзер файлов\n')
 
 path = sys.argv[1]
-text_ext = ['.txt', '.py', '.log', '.docx', '.doc', '.pdf', '.html']
-dirs = enumerate(os.listdir(path))
+os.chdir(path)
 
-def default_sort(path):
-	print(f'Вы выбрали фильтрацию по типу из {path}')
+files = os.listdir(path)
+
+sys.argv.append('--type')
+
+mode = sys.argv[2]
+# text_formats = ['.txt', '.py', '.log', '.docx', '.doc', '.pdf', '.html']
+
+def default_sort(unsorted_files,path):
+	display_text("ТИПУ ФАЙЛА", path)
+
+	files = sorted(unsorted_files, key=lambda file: os.path.splitext(file)[-1])
+
+	for index, file in enumerate(files):
+		sleep(1)
+		print(f"{index + 1}. {file}")
+
+def dry_run(unsorted_files, path):
+	display_text("умолчанию (беспорядочный вывод)", path)
+
+	for index, file in enumerate(unsorted_files):
+		sleep(1)
+		print(f"{index + 1}. {file}")
+
+def by_date(unsorted_files, path):
+	display_text("ДАТЕ ИЗМЕНЕНИЯ", path)
+
+	def sum_hours(file):
+		time = ctime(os.path.getmtime(file)).split()
+		months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',]
+
+		# Полный перевод в минуты из формата: Wed Sep 16 23:50:14 2026
+		summ_minutes = (((int(time[4]) * 365) * 24) * 60) + ((((months.index(time[1]) + 1) * 30) * 24) * 60) + ((int(time[2]) * 24) * 60)
+		minutes = [int(t) for t in time[3].split(':')]
+
+		return summ_minutes + (minutes[0] * 60) + minutes[1]
+
+	sort_files = sorted(unsorted_files, key=sum_hours)
+
+	for index, file in enumerate(sort_files):
+		sleep(1)
+		print(f"{index + 1}. {file.ljust(len(max(sort_files, key=len)))} | {ctime(os.path.getmtime(file))}")
+
+def by_size(unsorted_files, path):
+	display_text("РАЗМЕРУ", path)
+
+	sort_files = sorted(unsorted_files, key=lambda file: os.path.getsize(file))
+
+	for index, file in enumerate(sort_files):
+		sleep(1)
+		print(f"{index + 1}. {file.ljust(len(max(sort_files, key=len)))} | {os.path.getsize(file)}B")
+
+def display_text(filter_name, path):
+	print(f'Вы выбрали фильтрацию по {filter_name} из {path}')
 	sleep(0.5)
 	print('Ожидайте...')
 
-	dirs = sorted(os.listdir(path), key=lambda dir: os.path.splitext(dir)[-1])
+modes = {
+	'--type': 	 default_sort,
+	'--dry-run': dry_run,
+	'--by-date': by_date,
+	'--by-size': by_size,
+}
 
-	for index, dir in enumerate(dirs):
-		sleep(1)
-		print(f"{index + 1}. {dir}")
-
-def dry_run(path):
-	print(f'Вы выбрали простой вывод файлов из {path}')
-	sleep(0.5)
-	print('Ожидайте...')
-
-	for index, dir in dirs:
-		# ext = os.path.splitext(dir)[-1]
-		sleep(1)
-		print(f"{index + 1}. {dir}")
-
-		# if ext in text_ext:
-		# 	print(f'Type: {ext}')
-		# else:
-		# 	print()
-
-def by_date(unsorted_dirs, path):
-	print(f'Вы выбрали фильтрацию по дате из {path}')
-	sleep(0.5)
-	print('Ожидайте...')
-
-	def sum_hours(dir):
-		time = ctime(os.path.getmtime(dir)).split()
-		print(time)
-		input("...")
-
-		months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
-				  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-				]
-
-		hours_year = ((time[4] * 365) * 24) # hours
-		hours_month = ((months.index(time[1]) + 1) * 30) * 24 # hours
-		hours_days = int(time[2]) * 24 # hours
-
-		return (hours_year * 60) + (hours_month * 60) + (hours_days * 60)
-
-	unsorted_dirs = sorted(unsorted_dirs, key=sum_hours)
-
-	for index, dir in unsorted_dirs:
-		sleep(1)
-		print(f"{index + 1}. {dir}")
-
-if sys.argv[-1] == path:
-	default_sort(path)
-else:
-	option = sys.argv[2]
-
-	if option == '--dry-run':
-		dry_run(path)
-
-	elif option == '--by-date':
-		by_date(dirs, path)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# scan_folder_name = os.listdir(os.path.join('My_Files', 'TTT'))
-# scan_folder_name = os.path.split('My_Files/TTT/README.md')
-# scan_folder_name = os.path.basename('My_Files/TTT/tic_tac_toe.py')
-# scan_folder_name = os.path.abspath('My_Files/TTT') # Преобразует относительный путь, в полный
-# scan_folder_name = os.path.exists('My_Files/TTT') # Проверяет существует ли папка или файл указанные в пути
-# scan_folder_name = os.path.isfile('My_Files/TTT') # Проверяет является ли путь файлом
-# scan_folder_name = os.path.isdir('My_Files/TTT/tic_tac_toe.py') # Проверяется является ли путь директорией
-# scan_folder_name = os.path.splitext('My_Files/LA/log_analysis.py')
-# scan_folder_name = pathlib.Path('')
-# files_in_folder = os.listdir(scan_folder_name)
-# file_change_name = os.rename(input('Путь к исходному файлу: '), input('Новый путь/имя: '))
-
-# print(os.path.join('MyFiles', 'TTT'))
+modes[mode](files, path)
